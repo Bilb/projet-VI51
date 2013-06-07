5b4eb9f7db2fac34146852fd8248c62239a9abf4
daboooo <julien.uuuu@gmail.com>
2013-06-07 11:10:14 +0200

    Gestion des cooredonnée Coord et CellCoord

--- src/Lemming/Coord.java
+++ src/Lemming/Coord.java
@@ -3,27 +3,36 @@ package Lemming;
 public abstract class Coord {
 
 	private int x;
-
 	private int y;
+	
+	public Coord() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Coord(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
 	public int getX() {
-		return 0;
+		return x;
 	}
 
 	public int getY() {
-		return 0;
+		return y;
 	}
 
 	public void setX(int x) {
-
+		this.x = x;
 	}
 
 	public void setY(int y) {
-
+		this.y = y;
 	}
 
 	public void set(int x, int y) {
-
+		this.x = x;
+		this.y = y;
 	}
-
 }
